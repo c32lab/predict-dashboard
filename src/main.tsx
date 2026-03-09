eb88972d624de674bdfed9c694a438a396647c3c
amani-frontend-dev <frontend-dev@o3cloud.com>
2026-03-09 18:34:52 +0800

    feat: frontend error tracking and performance monitoring (issue #228)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { initErrorTracker } from './utils/errorTracker'
+import { initPerformanceMonitor } from './utils/performanceMonitor'
+
+initErrorTracker()
+initPerformanceMonitor()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
